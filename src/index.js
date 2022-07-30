31757d2f4e17f14f9bb93b237bde1a10f7c3253e
akarshippili <ippiliakarsh@gmail.com>
2022-07-31 00:08:16 +0530

    updated src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import config from './config/index.js';
+import { connection } from './database/index.js';
+import expressApp from './express-app.js';
 
-const startServer = () => {
+const startServer = async () => {
     const app = express();
+    await connection();
+    expressApp(app);
     app.listen(config.port, () => {
         console.log(`Server started on port ${config.port}`);
     });
