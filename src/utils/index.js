b35b25092fe52bcaa319f9c0a7101cee10db9812
akarshippili <ippiliakarsh@gmail.com>
2022-07-31 23:13:52 +0530

    updated utils with jwt based util functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import config from '../config/index.js';
 
 
 export const generateSalt = async () => {
@@ -13,4 +14,19 @@ export const hashPassword = async (password, salt) => {
 
 export const comparePassword = async (password, hashedPassword) => {
     return await bcrypt.compare(password, hashedPassword);
+}
+
+
+export const generateToken = (payload) => {
+    return jwt.sign(payload, config.secret, { expiresIn: config.jwtExpiration });
+}
+
+export const verifyToken = (token) => {
+    try {
+        const decoded = jwt.verify(token, config.secret);
+        return decoded;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
