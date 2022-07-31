4f7b5f6c7606979acdc827a33cceb1c11464eadd
akarshippili <ippiliakarsh@gmail.com>
2022-07-31 23:14:28 +0530

    added auth middleware

---
+++ src/apis/middlewares/auth.js
@@ -0,0 +1,18 @@
+import { verifyToken } from '../../utils/index.js';
+
+export const auth = async (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({
+            message: 'No token provided.'
+        });
+    }
+    const decoded = verifyToken(token);
+    if (!decoded) {
+        return res.status(401).json({
+            message: 'Invalid token.'
+        });
+    }
+    req.user = decoded;
+    next();
+};
