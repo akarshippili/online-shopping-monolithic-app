14f25065671738c143af7aa4295954446d02a692
akarshippili <ippiliakarsh@gmail.com>
2022-08-04 01:21:29 +0530

    updated product repo

--- src/database/repository/productRepository.js
+++ src/database/repository/productRepository.js
@@ -1,3 +1,35 @@
-import { ProductModel } from "../database/models/index.js";
+import { ProductModel } from "../models/index.js";
 
-export default class ProductRepository{}
+export default class ProductRepository{
+
+    async createProduct(product){
+        try {
+            const productModel = new ProductModel(product);
+            const newProduct = await productModel.save();
+            return newProduct;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getProductById(id){
+        try {
+            const product = await ProductModel.findById(id);
+            return product;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getProductsByCategory(category){
+        try {
+            const products = await ProductModel.find({category: category});
+            return products;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+}
