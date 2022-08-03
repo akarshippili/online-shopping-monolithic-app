b904a7fd1fb5da3f5d94a805ee09946dcd4aa985
akarshippili <ippiliakarsh@gmail.com>
2022-08-04 01:22:00 +0530

    updated product service

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,2 +1,52 @@
+import { ProductRepository } from "../database/repository/index.js";
+export default class ProductService {
 
-export default class ProductService {}
+    constructor() {
+        this.productRepository = new ProductRepository();
+    }
+
+    async createProduct(product) {
+        try {
+            const newProduct = await this.productRepository.createProduct(product);
+            return newProduct;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async createProducts(products) {
+        try {
+            let savedProducts = products.map(async (product) => this.productRepository.createProduct(product));
+            let reponseData = await Promise.all(savedProducts);
+            return reponseData;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            const product = await this.productRepository.getProductById(id);
+            if(!product) {
+                throw new Error("Product does not exist");
+            }
+            return product;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getProductsByCategory(category) {
+        try {
+            const products = await this.productRepository.getProductsByCategory(category);
+            return products;
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+}
